cc5538bbab7618aa3e60db72707c458a77e90b19
saniyaagrawal <saniya.agrawal30@gmail.com>
2020-12-10 15:52:04 +0530

    auth changes removed

--- index.js
+++ index.js
@@ -8,31 +8,31 @@ const billRouter=require('./routes/billRouter');
 const app=express();
 app.use(bodyParser.json());
 
-function auth (req, res, next) {
-    console.log(req.headers);
-    var authHeader = req.headers.authorization;
-    if (!authHeader) {
-        var err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');
-        err.status = 401;
-        next(err);
-        return;
-    }
+// function auth (req, res, next) {
+//     console.log(req.headers);
+//     var authHeader = req.headers.authorization;
+//     if (!authHeader) {
+//         var err = new Error('You are not authenticated!');
+//         res.setHeader('WWW-Authenticate', 'Basic');
+//         err.status = 401;
+//         next(err);
+//         return;
+//     }
   
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    var user = auth[0];
-    var pass = auth[1];
-    if (user == 'admin' && pass == 'password') {
-        next(); // authorized
-    } else {
-        var err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');      
-        err.status = 401;
-        next(err);
-    }
-  }
+//     var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+//     var user = auth[0];
+//     var pass = auth[1];
+//     if (user == 'admin' && pass == 'password') {
+//         next(); // authorized
+//     } else {
+//         var err = new Error('You are not authenticated!');
+//         res.setHeader('WWW-Authenticate', 'Basic');      
+//         err.status = 401;
+//         next(err);
+//     }
+//   }
 
-app.use(auth);
+// app.use(auth);
 
 app.use(express.static(path.join(_dirname, 'public')));
 
